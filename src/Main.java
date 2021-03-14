b212e671368d95a776f3f414591779c6ff301612
Zubareva-Anna <a84.zubareva@gmail.com>
2021-03-14 18:29:35 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 2_000_000_000;
-        int count = 500_000_000;
-        int total = price + count;
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
         System.out.printf("Total " + total);
 
     }
